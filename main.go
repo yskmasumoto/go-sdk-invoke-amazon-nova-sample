66c3d0bd06ebdb013e6634afb1e37f36bcc95c0b
yskmasumoto <msmtprogram0615@gmail.com>
2025-02-27 11:36:49 +0900

    add code

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+)
+
+func main() {
+	ctx := context.Background()
+	result, err := InvokeBedrock(ctx)
+	if err != nil {
+		slog.Error("failed to invoke bedrock", "error", err)
+	}
+
+	slog.Info("invoke bedrock result", "result", result)
+}
